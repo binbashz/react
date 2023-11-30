6d3ac2b88aae3c0fda493f3114be02bfc29458f1
binbashz <5732@holbertonstudents.com>
2023-11-30 16:48:52 -0300

    my first component x3

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -15,11 +20,23 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          aprendamos react
         </a>
       </header>
     </div>
   );
 }
 
+function TodoItem() {
+  return (
+    <li>
+      <span>V</span>
+      <p> Estudiar react</p>
+      <span>X</span>
+    </li>
+  );
+}
+
+
 export default App;
+
